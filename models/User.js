52799e871c95f8c15f0035873399be40533ac2a9
Ranajit <ranajitbangal01@gmail.com>
2025-02-09 18:23:37 +0530

    c

--- models/User.js
+++ models/User.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    firstName:{ type: String },
+    lastName:{ type: String},
+    mobile:{ type: Number},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'doctor', 'admin'], default: 'user' }
@@ -9,4 +12,3 @@ const UserSchema = new mongoose.Schema({
 
 
 module.exports = mongoose.model('User', UserSchema);
-
